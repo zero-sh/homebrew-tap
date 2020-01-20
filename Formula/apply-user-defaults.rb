0ee2c9e7b7015886dec97863c58dc89163d71965
Michael Sanders <michael.sanders@fastmail.com>
2020-01-20 14:19:31 -0800

    Update formula to build from source (pending Bintray)

--- Formula/apply-user-defaults.rb
+++ Formula/apply-user-defaults.rb
@@ -1,19 +1,19 @@
 class ApplyUserDefaults < Formula
-  VERSION = "0.1.1"
-
   desc "Small tool to set macOS user defaults from a YAML file"
   homepage "https://github.com/zero-sh/apply-user-defaults"
-  url "https://github.com/zero-sh/apply-user-defaults/releases/download/#{VERSION}/apply-user-defaults-#{VERSION}-x86_64-apple-darwin.tar.gz"
-  version VERSION
-  sha256 "9091f648e344419823af678600376ec40e7c3d2d54df58e6d93d552d80cb9124"
+  url "https://github.com/zero-sh/apply-user-defaults/archive/0.1.1.tar.gz"
+  sha256 "7c28c8791900fcdf076d2d42f92a1614c7268d5d9b649a34b36197f9ab94b4a8"
   head "https://github.com/zero-sh/apply-user-defaults.git"
 
-  def install
-    bin.install "apply-user-defaults"
+  depends_on "rust" => :build
 
-    bash_completion.install "complete/apply-user-defaults.bash"
-    fish_completion.install "complete/apply-user-defaults.fish"
-    zsh_completion.install "complete/_apply-user-defaults"
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+
+    out_dir = Dir["target/release/build/apply-user-defaults-*/out"].first
+    bash_completion.install "#{out_dir}/apply-user-defaults.bash"
+    fish_completion.install "#{out_dir}/apply-user-defaults.fish"
+    zsh_completion.install "#{out_dir}/_apply-user-defaults"
   end
 
   test do
