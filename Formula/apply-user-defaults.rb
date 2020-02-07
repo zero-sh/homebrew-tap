280fa0bfa2b79a1fcb2241744857a37680d4151e
Michael Sanders <michael.sanders@fastmail.com>
2020-02-07 12:09:35 -0800

    Update apply-user-defaults to 0.1.2

--- Formula/apply-user-defaults.rb
+++ Formula/apply-user-defaults.rb
@@ -1,11 +1,11 @@
 class ApplyUserDefaults < Formula
-  VERSION = "0.1.1"
+  VERSION = "0.1.2".freeze
 
   desc "Small tool to set macOS user defaults from a YAML file"
   homepage "https://github.com/zero-sh/apply-user-defaults"
   url "https://github.com/zero-sh/apply-user-defaults/releases/download/#{VERSION}/apply-user-defaults-#{VERSION}-x86_64-apple-darwin.tar.gz"
   version VERSION
-  sha256 "9091f648e344419823af678600376ec40e7c3d2d54df58e6d93d552d80cb9124"
+  sha256 "8329c20f7f4c8cc87372e4077411232224bcb01f87883ddd90786e7b52adc14a"
   head "https://github.com/zero-sh/apply-user-defaults.git"
 
   def install
