7314e646328cec60ff7119a73fe70ac2557854ed
Michael Sanders <michael.sanders@fastmail.com>
2020-06-07 07:32:23 -0700

    Add dependencies for zero formula

--- Formula/zero.rb
+++ Formula/zero.rb
@@ -7,6 +7,9 @@ class Zero < Formula
   version VERSION
   sha256 "4e708dab2a6ae71e011fbc6e8f4d12febc2d6502cfae0a46662f570003418bdc"
   head "https://github.com/zero-sh/zero.sh.git"
+  depends_on "apply-user-defaults"
+  depends_on "mas"
+  depends_on "stow"
 
   def install
     bin.install "zero"
