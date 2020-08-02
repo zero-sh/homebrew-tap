809f221c275b746c081f0825e678a9b02f76d09a
Michael Sanders <michael.sanders@fastmail.com>
2020-08-02 16:44:11 -0700

    Update zero to 0.4.0

--- Formula/zero.rb
+++ Formula/zero.rb
@@ -1,11 +1,11 @@
 class Zero < Formula
-  VERSION = "0.3.1".freeze
+  VERSION = "0.4.0".freeze
 
   desc "Radically simple personal bootstrapping tool for macOS"
   homepage "https://github.com/zero-sh/zero.sh"
   url "https://github.com/zero-sh/zero.sh/releases/download/#{VERSION}/zero-#{VERSION}.tar.gz"
   version VERSION
-  sha256 "8ce6b05d58ab00df922d0171d752534c3742b3b86cd3714ff892b27ccaffb8b1"
+  sha256 "20ec8b5520e4d411bc5e5ecc6bfaf7498a19d1699b9f1ca88d5901089e8601ff"
   head "https://github.com/zero-sh/zero.sh.git"
   depends_on "apply-user-defaults"
   depends_on "mas"
